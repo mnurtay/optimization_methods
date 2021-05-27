f56e135e32177963d1c9d0b4b0f9eebf50169848
Maksat Nurtay <nurtaims@gmail.com>
2021-05-27 11:02:39 +0600

    last commit

---
+++ week 7/lab 7.py
@@ -0,0 +1,17 @@
+# Lab 7
+# @Author: Nurtay Maksat
+# Многомерная Минимизация
+
+X = [1, 1]
+E = 0.5
+
+
+def f(x1, x2):
+    return 9 * x1**2 + x2**2
+
+
+def calculate():
+    x2 = X[1]
+
+
+calculate()
